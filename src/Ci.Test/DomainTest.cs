cae355a61dcb82732a4988fa862ef889fd1f2193
Joel Musheno <joelmusheno@gmail.com>
2016-12-04 16:26:26 -0500

    adding ref from proj to proj, and basic test to wire to ci pipeline

--- src/Ci.Test/DomainTest.cs
+++ src/Ci.Test/DomainTest.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CiDomain;
+using NUnit.Framework;
 
 namespace Ci.Test
 {
-    public class Class1
-    {
-        public Class1()
+    [TestFixture]
+    public class DomainTest
+    { 
+        [Test]
+        public void ShouldInstanciate()
         {
+            var dl = new DomainLogic();
+            Assert.IsNotNull(dl);
         }
     }
 }
